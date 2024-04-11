c8e3dee22340b76d6179baba298a8acae2ca1938
edpyt <edrsa1321s@gmail.com>
2024-04-11 19:19:34 +0300

    setup alembic

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,13 @@ ENV PYTHONUNBUFFERED 1
 COPY ./pyproject.toml /app/
 COPY ./src /app/src
 
+FROM build as dev
 RUN python -m pip install uv
 RUN uv pip compile pyproject.toml -o requirements.txt
 RUN uv pip install --no-cache \
                       --python $(which python3.11) \
                       -r requirements.txt
+
+FROM build as migrations
+RUN python -m pip install poetry
+RUN poetry install --only db
--- src/infrastructure/persistence/repositories/menu_repo.py
+++ src/infrastructure/persistence/repositories/menu_repo.py
@@ -1,11 +1,14 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.application.persistence.menu_repo import MenuRepository
-from src.domain.menu.menu import Menu
+from src.infrastructure.persistence.db.models.menu import Menu
 
 
 class MenuRepositoryImpl(MenuRepository):
     def __init__(self, session: AsyncSession) -> None:
         self.session = session
 
-    async def add(self, menu: Menu) -> None: ...
+    # FIXME: type error
+    async def add(self, menu: Menu) -> None:  # type: ignore[override]
+        self.session.add(menu)
+        await self.session.commit()
