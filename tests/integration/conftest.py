d34e4cf7357f73083551486de596f5bdef81516c
edpyt <edrsa1321s@gmail.com>
2024-04-18 18:59:09 +0300

    fix: update db migrations setup

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,9 +1,11 @@
 from typing import AsyncGenerator, Callable, Coroutine, Generator
 
 import pytest
+from alembic.command import upgrade
+from alembic.config import Config as AlembicConfig
 from blacksheep import Application, JSONContent
 from blacksheep.testing import TestClient
-from sqlalchemy import create_engine
+from sqlalchemy import Connection
 from sqlalchemy.ext.asyncio import (
     AsyncEngine,
     AsyncSession,
@@ -16,7 +18,6 @@ from src.application.persistence.menu_repo import MenuRepository
 from src.application.persistence.user_repo import UserRepository
 from src.infrastructure.config.db import DBConfig
 from src.infrastructure.config.jwt import JWTConfig
-from src.infrastructure.persistence.db.models.base import Base
 from src.infrastructure.persistence.repositories.menu_repo import MenuRepositoryImpl
 from src.infrastructure.persistence.repositories.user_repo import UserRepositoryImpl
 from testcontainers.postgres import PostgresContainer
@@ -38,6 +39,13 @@ def create_db_config(postgres_db: PostgresContainer) -> DBConfig:
         password="test",
         port=postgres_db.get_exposed_port("5432"),
     )
+
+
+@pytest.fixture(name="alembic_config", scope="session")
+def create_alembic_config(db_config: DBConfig) -> AlembicConfig:
+    alembic_cfg = AlembicConfig("alembic.ini")
+    alembic_cfg.set_main_option("sqlalchemy.url", db_config.full_url)
+    return alembic_cfg
 
 
 @pytest.fixture(name="jwt_config", scope="session")
@@ -71,23 +79,28 @@ def test_client(app: Application, di_overrides: list[DIOverride]) -> TestClient:
     return client
 
 
+@pytest.fixture(scope="session")
+async def _run_db_migrations(alembic_config: AlembicConfig) -> None:
+    def migrate_db(connection: Connection) -> None:
+        nonlocal alembic_config
+
+        alembic_config.attributes["connection"] = connection
+        upgrade(alembic_config, "head")
+
+    migration_engine = create_async_engine(alembic_config.get_main_option("sqlalchemy.url"))
+    async with migration_engine.connect() as conn:
+        await conn.run_sync(migrate_db)
+    await migration_engine.dispose()
+
+
 @pytest.fixture(name="sa_engine", scope="session")
-async def create_sa_engine(db_config: DBConfig) -> AsyncGenerator[AsyncEngine, None]:
+async def create_sa_engine(
+    db_config: DBConfig,
+    _run_db_migrations: None,
+) -> AsyncGenerator[AsyncEngine, None]:
     engine = create_async_engine(db_config.full_url)
     yield engine
     await engine.dispose()
-
-
-@pytest.fixture(scope="session", autouse=True)
-def _run_db_migrations(db_config: DBConfig) -> Generator[None, None, None]:
-    migration_engine = create_engine(db_config.full_url.replace("+asyncpg", ""))
-
-    with migration_engine.begin():
-        Base.metadata.create_all(migration_engine)
-        yield
-        Base.metadata.drop_all(migration_engine)
-
-    migration_engine.dispose()
 
 
 @pytest.fixture(name="sa_session_factory", scope="session")
