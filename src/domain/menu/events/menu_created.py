f3447c836dad1688a26bef81fe59fda2334d7634
edpyt <edrsa1321s@gmail.com>
2024-04-26 21:24:42 +0300

    fix: circular import error

--- src/domain/menu/events/menu_created.py
+++ src/domain/menu/events/menu_created.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass
+from typing import TypeVar
 
 from src.domain.common.models.domain_event import DomainEvent
-from src.domain.menu.menu import Menu
+
+Menu = TypeVar("Menu")
 
 
 @dataclass
 class MenuCreated(DomainEvent):
-    menu: Menu
+    menu: Menu  # type: ignore
--- src/infrastructure/services/dt_provider.py
+++ src/infrastructure/services/dt_provider.py
@@ -1,8 +1,8 @@
-from datetime import datetime
+from datetime import UTC, datetime
 
 from src.application.common.interfaces import DateTimeProvider
 
 
 class DateTimeProviderImpl(DateTimeProvider):
     def utc_now(self) -> datetime:
-        return datetime.now()  # noqa: DTZ005
+        return datetime.now(tz=UTC)
