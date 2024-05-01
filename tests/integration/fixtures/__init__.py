3871426c269cfa6797f3cb691753d73b129a55ac
edpyt <edrsa1321s@gmail.com>
2024-05-01 12:11:47 +0300

    kruto

--- tests/integration/fixtures/__init__.py
+++ tests/integration/fixtures/__init__.py
@@ -2,7 +2,7 @@ from .api import create_auth_token, di_overrides, test_client
 from .db.db import create_db_config, create_postgres_db
 from .db.migrations import create_alembic_config, run_db_migrations
 from .db.sqla import create_async_sa_session, create_sa_session_factory, create_test_sa_engine
-from .events import create_event_bus, create_nats_container
+from .events import create_event_bus, create_nats_container, menu_create_handler
 from .repositories import create_menu_repository, create_user_factory, create_user_repository
 
 __all__ = (
@@ -21,4 +21,5 @@ __all__ = (
     "create_user_factory",
     "create_event_bus",
     "create_nats_container",
+    "menu_create_handler",
 )
